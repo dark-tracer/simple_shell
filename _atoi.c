c0842f622ee2c41357870207d71e5995d4006083
dark-tracer <bernieamponsah2@gmail.com>
2023-12-13 00:12:11 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,22 +1,24 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- *
- * Return: 1 if interactive mode, 0 otherwise
+ *interactive - returns true if shell is interactive mode
+ *@info: struct address
+ *Return: 1 if interactive mode, 0 otherwise
  */
+
 int interactive(info_t *info)
+
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ *is_delim - checks if character is a delimeter
+ *@c: the char to check
+ *@delim: the delimeter string
+ *Return: 1 if true, 0 if false
  */
+
 int is_delim(char c, char *delim)
 {
 	while (*delim)
@@ -26,12 +28,13 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
 
 int _isalpha(int c)
+
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
@@ -40,27 +43,30 @@ int _isalpha(int c)
 }
 
 /**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
 
 int _atoi(char *s)
+
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+
 	{
 		if (s[i] == '-')
 			sign *= -1;
-
 		if (s[i] >= '0' && s[i] <= '9')
+
 		{
 			flag = 1;
 			result *= 10;
 			result += (s[i] - '0');
 		}
+
 		else if (flag == 1)
 			flag = 2;
 	}
@@ -69,6 +75,5 @@ int _atoi(char *s)
 		output = -result;
 	else
 		output = result;
-
 	return (output);
 }
